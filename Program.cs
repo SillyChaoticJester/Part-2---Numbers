9570f2d171ad4f1c238d76c480ef3e0c5631ef79
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-05 14:03:40 -0500

    Putting down info

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 18;
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            Console.WriteLine(a/3);
+            Console.WriteLine(19 / 3);
+            Console.WriteLine(23 / 3);
+            Console.WriteLine(24 / 3);
+            Console.WriteLine(19 / 10);
+
+            //Things used for Numbers
+            //Addition: +
+            //Subtraction: -
+            //Multiplication: *
+            //Division: /
+            //Decimals (outside of doubles): 1.0
+            //Remainder: %
+            //Decimals (as a Value): 1.0M
+
+            //1. Double
+            //2. When dividing an int, it only uses the first number of a decimal, while a double uses the full decimal
+            //3a. 2
+            //3b. 2.5
+            //3c. 2.5
+            //3d. 2.5
         }
     }
 }
